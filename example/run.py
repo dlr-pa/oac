a99c42ad015dc7046d39367530ffee0be4f3e6fd
Liam Megill <liam.megill@dlr.de>
2024-11-07 10:47:24 +0100

    Modified example.toml and reference to PATH change

--- example/run.py
+++ example/run.py
@@ -1,5 +1,10 @@
 """Demonstration of OpenAirClim simulation run"""
 
+# if you have not added the oac folder to your PATH, then you also need to
+# import sys and append to PATH using sys.path.append(`.../oac`)
+import os
 import openairclim as oac
 
+# change directory to match current file
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 oac.run("example.toml")
