3f1af29b0bec7be6e2ebd72f651520539f4abfbe
Stefan Voelk <stefan.voelk@dlr.de>
2026-03-19 13:08:55 +0100

    Update CHANGELOG for version 0.16.0
    Update CHANGELOG and __about__.py

--- openairclim/__about__.py
+++ openairclim/__about__.py
@@ -12,9 +12,9 @@ __all__ = [
 
 
 __title__ = "OpenAirClim"
-__version__ = "0.15.0"
+__version__ = "0.16.0"
 __author__ = "OpenAirClim Team"
 __email__ = "openairclim@dlr.de"
 __license__ = "Apache 2.0"
-__copyright__ = f"2025, {__author__}"
+__copyright__ = f"2026, {__author__}"
 __url__ = "https://github.com/dlr-pa/oac"
