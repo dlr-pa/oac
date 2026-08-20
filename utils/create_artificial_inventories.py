5171aaf4979dd4f0fdb234356260b20ed7a1c6fa
Liam Megill <liam.megill@dlr.de>
2026-08-20 17:15:31 +0200

    Update create_artificial_inventories

--- utils/create_artificial_inventories.py
+++ utils/create_artificial_inventories.py
@@ -317,9 +317,9 @@ def plot_sample_emission_inventory(rnd_inv_dict):
 
 
 if __name__ == "__main__":
-    import openairclim as oac
+    from openairclim.core.plot import plot_inventory_vertical_profiles
 
-    art_inv_dict = ArtificialInventoryDict(year_arr=YEAR_ARR).create()
+    art_inv_dict = ArtificialInventoryDict(year_arr=YEAR_ARR, ac_lst=["RJ", "NB", "WB"]).create()
     convert_xr_dict_to_nc(art_inv_dict)
-    oac.plot_inventory_vertical_profiles(art_inv_dict)
+    plot_inventory_vertical_profiles(art_inv_dict)
     plot_sample_emission_inventory(art_inv_dict)
