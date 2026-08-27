dlr-pa/oac

commit 5171aaf4979dd4f0fdb234356260b20ed7a1c6fa

Update create_artificial_inventories

utils/create_artificial_inventories.py
@@ -317,9 +317,9 @@ def plot_sample_emission_inventory(rnd_inv_dict):
 
 
 if __name__ == "__main__":
-    import openairclim as oac
+    from openairclim.core.plot import plot_inventory_vertical_profiles
 
-    art_inv_dict = ArtificialInventoryDict(year_arr=YEAR_ARR).create()
+    art_inv_dict = ArtificialInventoryDict(year_arr=YEAR_ARR, ac_lst=["RJ", "NB", "WB"]).create()
     convert_xr_dict_to_nc(art_inv_dict)
-    oac.plot_inventory_vertical_profiles(art_inv_dict)
+    plot_inventory_vertical_profiles(art_inv_dict)
     plot_sample_emission_inventory(art_inv_dict)

commit b930846b2084bf5fc173baf9df392832180839db

Remove ac in standard artificial invs Commit 5171aaf accidentally added the optional ac_lst variable to the default ArtificialInventoryDict call, which is used by new users when creating test data. Since the aircraft in ac_lst were not defined in the example config, it produced an error. This commit removes ac_lst.

utils/create_artificial_inventories.py
@@ -319,7 +319,7 @@ def plot_sample_emission_inventory(rnd_inv_dict):
 if __name__ == "__main__":
     from openairclim.core.plot import plot_inventory_vertical_profiles
 
-    art_inv_dict = ArtificialInventoryDict(year_arr=YEAR_ARR, ac_lst=["RJ", "NB", "WB"]).create()
+    art_inv_dict = ArtificialInventoryDict(year_arr=YEAR_ARR).create()
     convert_xr_dict_to_nc(art_inv_dict)
     plot_inventory_vertical_profiles(art_inv_dict)
     plot_sample_emission_inventory(art_inv_dict)